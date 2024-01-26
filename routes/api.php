2bee4302975b88d80cd61bc8a89a109663060a20
PapriBiswas2023 <official.p2023@gmail.com>
2024-01-26 19:15:20 +0530

    login

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ use App\Http\Controllers\UserController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/register',[UserController::class,'register']);
+Route::post('register',[UserController::class,'register']);
+Route::post('login',[UserController::class,'login']);
